bf0b4a403af68857b2a6bb053baec01fe454becc
khinikadze-g <khinikadzegiga@gmail.com>
2026-05-06 14:39:09 +0400

    base62 encoding

---
+++ tests/UrlShortener.Api.Core.Test/Base62EncodingScenrios.cs
@@ -0,0 +1,17 @@
+﻿using UrlShortener.Core;
+
+namespace UrlShortener.Api.Core.Test
+{
+    public class Base62EncodingScenrios
+    {
+        [Theory]
+        [InlineData(20, "K")]
+        [InlineData(1, "1")]
+        public void Should_Encode_Number_To_Base62(int number, string expected)
+        {
+            number.EncodeToBase62().
+                Should().
+                Be(expected);
+        }
+    }
+}
